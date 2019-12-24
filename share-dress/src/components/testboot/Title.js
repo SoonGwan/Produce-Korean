d7fc660338120a456539488860113f17ccd6c013
SoonGwan <gamawoo10@gmail.com>
2019-12-24 11:06:34 +0900

    Update Title.js

--- share-dress/src/components/testboot/Title.js
+++ share-dress/src/components/testboot/Title.js
@@ -6,14 +6,16 @@ function Title() {
       <div class="sp-container">
         <div class="sp-content">
           <div class="sp-globe"></div>
-          <h2 class="frame-1">가나다라마바사</h2>
-          <h2 class="frame-2">머라고 써야되지</h2>
+          <h2 class="frame-1">Who says the nights are for sleeping</h2>
+          <h2 class="frame-2">
+            I'm not lazy <br /> I'm just on my energy saving mode
+          </h2>
           <h2 class="frame-3">ㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ</h2>
           <h2 class="frame-4">:ㅇㅇㅇㅇㅇㅇ4</h2>
           <h2 class="frame-5">
-            <span>5,</span>
-            <span>6,</span>
-            <span>7.</span>
+            <span>NEVER </span>
+            <span>SAY </span>
+            <span>NEVER</span>
           </h2>
         </div>
       </div>
